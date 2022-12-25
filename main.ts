25a2dbe661161f16fc3dad85db304bd83404fc5d
417242 <115142420+417242@users.noreply.github.com>
2022-12-25 20:28:10 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ let X = 0
 let Y = 0
 let list: number[] = []
 let n = 0
+let 亮燈數 = 0
 let a = 0
 function 燈數 (num: number) {
     if (num % 5 == 0) {
@@ -20,13 +21,20 @@ input.onButtonPressed(Button.A, function () {
         list.push(0)
     }
     n = 0
-    while (n < 3) {
+    while (n < 亮燈數) {
         a = randint(0, 24)
         if (list[a] == 0) {
+            list[a] = 1
             n += 1
             燈數(a + 1)
         }
     }
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    亮燈數 += 1
 })
 basic.forever(function () {
 	
